540cf12a8c6d67356a368412d2af5b089b32a1bb
siahuat0727 <tansiahuat@gmail.com>
2018-12-09 14:39:33 +0800

    Clean code

--- leetcode/2.add-two-numbers.cpp
+++ leetcode/2.add-two-numbers.cpp
@@ -32,38 +32,24 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
  * };
  */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *ans = NULL;
+        ListNode *ans = nullptr;
         ListNode **temp = &ans;
         int carry = 0;
-        while (l1 != NULL && l2 != NULL) {
-            int sum = l1->val + l2->val + carry;
+        while (l1 != nullptr || l2 != nullptr) {
+            int val1 = l1 ? l1->val : 0;
+            int val2 = l2 ? l2->val : 0;
+            int sum = val1 + val2 + carry;
             carry = (int)(sum >= 10);
             int digit = carry ? sum - 10 : sum;
             *temp = new ListNode(digit);
-            l1 = l1->next;
-            l2 = l2->next;
-            temp = &((*temp)->next);
-        }
-        while (l1 != NULL) {
-            int sum = l1->val + carry;
-            carry = (int)(sum >= 10);
-            int digit = carry ? sum - 10 : sum;
-            *temp = new ListNode(digit);
-            l1 = l1->next;
-            temp = &((*temp)->next);
-        }
-        while (l2 != NULL) {
-            int sum = l2->val + carry;
-            carry = (int)(sum >= 10);
-            int digit = carry ? sum - 10 : sum;
-            *temp = new ListNode(digit);
-            l2 = l2->next;
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;
             temp = &((*temp)->next);
         }
         if (carry) {
